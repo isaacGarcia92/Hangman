8d6a012d32417f257ba2b38fa10e46d7bdbc1e37
isaacGarcia92 <isaacvinicio.garcia@gmail.com>
2022-11-30 06:30:04 -0600

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Hangman;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> wordsList = new List<string>() { "House", "Phone", "Water", "Food", "Banana" };
+        Console.WriteLine(HangmanListMethods.getRandomWord(wordsList));
+    }
+}
