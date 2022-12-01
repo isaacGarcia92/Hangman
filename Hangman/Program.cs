2c77d6a42a83ee25040860c9c9000e75e54b9436
isaacGarcia92 <isaacvinicio.garcia@gmail.com>
2022-11-30 20:39:35 -0600

    Testing if user letter input exists on random word.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -7,10 +7,26 @@ class Program
     static void Main(string[] args)
     {
         // Creating a List
-        List<string> wordsList = new List<string>() { "House", "Phone", "Water", "Food", "Banana" };
+        List<string> wordsList = new List<string>() { "HOUSE", "PHONE", "WATER", "FOOD", "BANANA" };
 
         // Random index number generator
         Random rand = new Random();
         int randomIndex = rand.Next(0, wordsList.Count);
+        string randomWord = wordsList[randomIndex];
+
+        // Getting user input
+        Console.WriteLine("Write letter:");
+        string userLetter = Console.ReadLine().ToUpper();
+
+        // Testing is user input exists on random word
+        if(randomWord.Contains(userLetter))
+        {
+            Console.WriteLine($"Letter {userLetter} was found on the random word");
+        }
+        else
+        {
+            Console.WriteLine($"Letter {userLetter} was not found on the random word");
+        }
+    
     }
 }
