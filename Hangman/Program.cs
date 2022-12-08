2233a62a40a136a666fae624e0fab272279644ec
isaacGarcia92 <isaacvinicio.garcia@gmail.com>
2022-12-07 20:02:35 -0600

    Adding Console Clear method and user chances tracker.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 class Program
 {
+    const int NUM_OF_CHANCES = 5;
     static void Main(string[] args)
     {
         // Creating a List
@@ -17,8 +18,12 @@ class Program
         // Key declaration
         ConsoleKey letter;
 
-        // Getting user input
-        Console.WriteLine("Write letter or press Enter to exit:");
+        // Introduction text and getting user input
+        Console.WriteLine($"Let's play the hangman game! You have {NUM_OF_CHANCES} chances to win");
+        Console.WriteLine("Write a letter or press Enter to exit:");
+
+        // User chances
+        int userChances = NUM_OF_CHANCES;
 
         // Guessed letters list
         List<string> guessedLetters = new List<string>();
@@ -26,14 +31,19 @@ class Program
         do
         {
             letter = Console.ReadKey().Key;
-            Console.WriteLine();
+            Console.Clear();
             guessedLetters.Add(letter.ToString());
 
             // Counter tracker
             int counter = 0;
 
-            if (letter == ConsoleKey.Enter) break;
+            if (letter == ConsoleKey.Enter)
+            {
+                Console.WriteLine("Thanks for playing!");
+                break;
+            };
 
+            // Evaluating if letters list contain every random word letter.
             for (int i=0; i<randomWord.Length; i++)
             {
 
@@ -42,16 +52,35 @@ class Program
                     Console.Write(randomWord[i] + " ");
                     counter++;
                 }
+                
                 if (!guessedLetters.Contains(randomWord[i].ToString()))
                 {
                     Console.Write("_" + " ");
                 }
+                
+                // End of program if all letters are guessed.
                 if (counter == randomWord.Length)
                 {
                     Console.WriteLine();
-                    Console.WriteLine("Congratulations, you guessed the word!");
+                    Console.WriteLine("Congratulations, you guessed the word! Thanks for playing!");
                     return;
                 }
+            }
+
+            // Displaying user chances tracker.
+            Console.WriteLine();
+            if (!randomWord.Contains(letter.ToString()))
+            {
+                Console.WriteLine("Wrong guess!");
+                userChances--;
+                Console.WriteLine($"You have {userChances} chances left");
+            }
+            
+            // End of program if user runs out of guesses.
+            if (userChances <= 0)
+            {
+                Console.WriteLine("You loose! Thanks for playing!");
+                break;
             }
             Console.WriteLine();
         }
